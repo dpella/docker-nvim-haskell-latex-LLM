390c81e20c4f2cb7d007a2316a720cf6c2f2a550
Alejandro Russo <alejandro@dpella.io>
2024-04-28 22:55:37 +0200

    Haskell auto-indent

--- otherfiles/init.lua
+++ otherfiles/init.lua
@@ -1,43 +1,43 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
 
-
-  -- Installing Haskell tools   
+  -- Installing Haskell tools
   {
     "mrcjkb/haskell-tools.nvim",
-    version = '^3', -- Recommended
+    version = "^3", -- Recommended
     lazy = false, -- This plugin is already lazy
   },
 
-  -- It describes what to install when running :MasonInstallAll 
+  -- It describes what to install when running :MasonInstallAll
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server",
-  			"stylua",
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua", -- Lua identation
+        "ormolu", -- Haskell identation
+      },
+    },
   },
 
-  -- Maximizes a split for a moment 
+  -- Maximizes a split for a moment
   {
-  	"szw/vim-maximizer",
-  	keys = {
-        { "<leader>mm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split"},
-  	},
+    "szw/vim-maximizer",
+    keys = {
+      { "<leader>mm", "<cmd>MaximizerToggle<CR>", desc = "Maximize/minimize a split" },
+    },
   },
 
   -- Nicer text input in the file tree
   {
-  	"stevearc/dressing.nvim",
-  	event = "VeryLazy",
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
   },
 
   -- To highlight TODO: comments and others alike, e.g, BUG: , HACK: , etc.
@@ -46,35 +46,32 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     lazy = false,
     config = function()
-        require("base46").load_all_highlights()
+      require("base46").load_all_highlights()
 
-        local todo_comments = require("todo-comments")
+      local todo_comments = require "todo-comments"
 
-        todo_comments.setup({
-                signs = false,
-            })
+      todo_comments.setup {
+        signs = false,
+      }
 
-        local map = vim.keymap.set
+      local map = vim.keymap.set
 
-         map("n", "]t", function()
-             todo_comments.jump_next()
-         end, { desc = "Next todo comment" })
+      map("n", "]t", function()
+        todo_comments.jump_next()
+      end, { desc = "Next todo comment" })
 
-         map("n", "[t", function()
-             todo_comments.jump_prev()
-         end, { desc = "Previous todo comment" })
-
+      map("n", "[t", function()
+        todo_comments.jump_prev()
+      end, { desc = "Previous todo comment" })
     end,
     event = "BufReadPost",
- },
+  },
 
- -- To surround text and then add quotes like "", e.g., config ysiw" -> "config"
- {
+  -- To surround text and then add quotes like "", e.g., config ysiw" -> "config"
+  {
     "kylechui/nvim-surround",
     event = { "BufReadPre", "BufNewFile" },
     version = "*",
     config = true,
- },
-
- 
+  },
 }
