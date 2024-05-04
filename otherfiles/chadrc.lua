1a4637148bf3fa6112be95793cb2162b124c0534
Alejandro Russo <alejandro@dpella.io>
2024-05-05 00:29:10 +0200

    DPella sign

--- otherfiles/chadrc.lua
+++ otherfiles/chadrc.lua
@@ -14,6 +14,28 @@ M.ui = {
   -- 	Comment = { italic = true },
   -- 	["@comment"] = { italic = true },
   -- },
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+       "     _            _ _        ",
+       "  __| |_ __   ___| | | __ _  ",
+       " / _` | '_ \\ / _ \\ | |/ _` | ",
+       "| (_| | |_) |  __/ | | (_| | ",
+       " \\__,_| .__/ \\___|_|_|\\__,_| ",
+       "      |_|                    ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 }
 
 return M
+
