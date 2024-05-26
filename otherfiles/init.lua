6fbdaf6258cd78ab86c93aaac9f4fa155b08be31
Alejandro Russo <alejandro@dpella.io>
2024-05-26 23:36:17 +0200

    Bibligraphy included when \cite{}

--- otherfiles/init.lua
+++ otherfiles/init.lua
@@ -169,4 +169,13 @@ return {
       }
     end,
   },
+
+  -- Telescope search for bibtex entries <C-e>
+  {
+    "nvim-telescope/telescope-bibtex.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").load_extension "bibtex"
+    end,
+  },
 }
