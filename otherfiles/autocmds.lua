5450a78c9eaae2baadab24702da2f68d71fc928c
Alejandro Russo <alejandro@dpella.io>
2025-05-04 13:11:35 +0200

    Color with textwidth

--- otherfiles/autocmds.lua
+++ otherfiles/autocmds.lua
@@ -4,6 +4,7 @@ autocmd("FileType", {
   pattern = "markdown",
   callback = function()
     vim.opt_local.textwidth = 80
+    vim.opt_local.colorcolumn = "80"
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
   end,
