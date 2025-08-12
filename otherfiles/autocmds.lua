cf1cd0515a412fc71cc1a007a6c234109e386736
Alejandro Russo <alejandro@dpella.io>
2025-08-12 10:22:08 +0200

    Personal prompts

--- otherfiles/autocmds.lua
+++ otherfiles/autocmds.lua
@@ -9,3 +9,28 @@ autocmd("FileType", {
     vim.opt_local.linebreak = true
   end,
 })
+
+require("codecompanion").setup {
+  prompt_library = {
+    ["Improve English"] = {
+      strategy = "inline",
+      description = "Improves sentences, paragraphs",
+      opts = {
+        adapter = {
+          name = "openai",
+          model = "gpt-4.1-mini",
+        },
+      },
+      prompts = {
+        {
+          role = "system",
+          content = "You are a native English speaker writting academic text",
+        },
+        {
+          role = "user",
+          content = "Make the following text flow better",
+        },
+      },
+    },
+  },
+}
